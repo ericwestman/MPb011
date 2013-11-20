82c51488b07238f5fc1eacf32b51c63b16a197c2
Sarah Strohkorb <sarah.strohkorb@gmail.com>
2013-11-19 19:18:34 -0500

    Started to comment it up

--- test_assembly.asm
+++ test_assembly.asm
@@ -1,10 +1,24 @@
-# Eric's code
+# Lab 03 Test Code
+# Shivam Desai, Kris Groth, Sarah Strohkorb, Eric Westman
+
+# Load the value of 1  and 2 from memory into registers $t1 and $t2 respectively
 lw $t1, 1021
 lw $t2, 1022
+
+# Store the value of $t1 into memory
 sw $t1, 1023
+
+# xor the value stored in $t1 (1) with 99
+# expected output: 98
 xori $t3, $t1, 99
+
+# $t1 + $t2 = 1 + 2 = 3, stored in $t4
 add $t4, $t1, $t2
+
+# $t2 - $t1 = 2 - 1 = 1, stored in $t5
 sub $t5, $t2, $t1
+
+# ($t1 < $t2) = (2 < 1 )= 1, stored into $t6
 slt $t6, $t1, $t2
 
 bne $t1, $t2, jumpelseif
